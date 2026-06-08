e3d4c70cdb4486e3ce542df068f6ea66e91589a6
Maazhassan486 <maazhassan486@gmail.com>
2026-06-08 12:54:35 +0500

    Fix CORS origin

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: "https://video-conferencing-app-two-nu.vercel.app/"
+  origin: "https://video-conferencing-app-two-nu.vercel.app"
 }));
 app.use(express.json());
 
